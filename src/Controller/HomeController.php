4cda38b2c8ab48dfa71ba783107b88a10a03a54c
DESKTOP-J9ISOD9\Mihai <nicolae.mihai.s9d@student.ucv.ro>
2021-10-31 10:45:20 +0200

    no message

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    #[Route('/',name:'index')]
+    public function index(): Response
+    {
+       
+        return $this->render('base.html.twig');
+    }
+}
